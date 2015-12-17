6a77a1c3c1695ae177d6dc06502f5c10d9d44c53
Keith Resar <keith.resar@centurylinkcloud.com>
2015-12-17 16:51:54 -0600

    v2.38 reword api exception

--- src/clc/APIv2/api.py
+++ src/clc/APIv2/api.py
@@ -130,7 +130,7 @@ class API():
 		else:
 			try:
 				e = clc.APIFailedResponse("Response code %s.  %s %s %s" % 
-				                          (r.status_code,r.json()['message'],method,"%s/%s" % (clc.defaults.ENDPOINT_URL_V2,url)))
+				                          (r.status_code,r.json()['message'],method,fq_url))
 				e.response_status_code = r.status_code
 				e.response_json = r.json()
 				e.response_text = r.text
@@ -139,7 +139,7 @@ class API():
 				raise
 			except:
 				e = clc.APIFailedResponse("Response code %s. %s. %s %s" % 
-				                         (r.status_code,r.text,method,"%s/%s" % (clc.defaults.ENDPOINT_URL_V2,url)))
+				                         (r.status_code,r.text,method,fq_url))
 				e.response_status_code = r.status_code
 				e.response_json = {}	# or should this be None?
 				e.response_text = r.text
--- src/setup.py
+++ src/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "clc-sdk",
-	version = "2.37",
+	version = "2.38",
 	packages = find_packages("."),
 
 	install_requires = ['prettytable','clint','argparse','requests'],
