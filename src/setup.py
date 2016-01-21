767d257bf57c44deb66d4ecc9c64d1ae0694e865
Keith Resar <keith.resar@centurylinkcloud.com>
2016-01-21 14:34:18 -0600

    v2.41 Multi-NIC support

--- src/setup.py
+++ src/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "clc-sdk",
-	version = "2.40",
+	version = "2.41",
 	packages = find_packages("."),
 
 	install_requires = ['prettytable','clint','argparse','requests'],
