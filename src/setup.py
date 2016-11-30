ae6e1f01257ba344722e496d41436bc3b6378526
Albert Choi <albert.choi@gmail.com>
2016-11-30 08:25:19 -0800

    bump to 2.46 and remove dist tarballs

--- src/setup.py
+++ src/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "clc-sdk",
-	version = "2.45",
+	version = "2.46",
 	packages = find_packages("."),
 
 	install_requires = ['prettytable','clint','argparse','requests'],
