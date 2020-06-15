a8e6bbd346f87ad1d20616a8482674db471b6c4f
Shi Jin <Shi.Jin@ctl.io>
2020-06-15 21:30:19 +0000

    bump version to 2.50

--- src/setup.py
+++ src/setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "clc-sdk",
-	version = "2.49",
+	version = "2.50",
 	packages = find_packages("."),
 
 	install_requires = ['prettytable','clint','argparse','requests'],
